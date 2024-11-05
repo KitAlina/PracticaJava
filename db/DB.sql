3a2e2786fa67c294bd276842066ab66d8730baa9
KitAlina <malinysik11@gmail.com>
2024-11-05 10:22:08 +0300

    add db

--- db/DB.sql
+++ db/DB.sql
@@ -10,7 +10,8 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema vetcli
 -- -----------------------------------------------------
-
+create database if not exists vetcli;
+use vetcli;
 -- -----------------------------------------------------
 -- Schema vetcli
 -- -----------------------------------------------------
